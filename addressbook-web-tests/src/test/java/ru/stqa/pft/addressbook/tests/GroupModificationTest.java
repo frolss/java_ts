8448202da5aae5091412c3baf7aa39d6620778c3
frolss <luppovfrol@live.ru>
2021-04-07 18:11:43 +0300

    add 3.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -8,6 +8,9 @@ public class GroupModificationTest extends TestBase {
     @Test
     public void GroupModification(){
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("wew", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("wew", "ewe", "qwq"));
